b96a34a65e26c0b4f525c4970793fb18295d8f24
davidmuo <d.muotohfra@alustudent.com>
2024-08-08 18:37:51 +0000

    d

---
+++ python-network_0/0-body_size.sh
@@ -0,0 +1,5 @@
+i#!/bin/bash
+# Display size of body of response; Usage: ./0-body_size.sh 0.0.0.0:5000
+curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' 'i#!/bin/bash
+# Display size of body of response; Usage: ./0-body_size.sh 0.0.0.0:5000
+curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '
