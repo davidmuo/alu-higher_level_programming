117003deeeae12021176cc20479da3c549444983
davidmuo <d.muotohfra@alustudent.com>
2024-09-29 09:17:22 -0700

    Update 10-factorial.js

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,9 +1,15 @@
 #!/usr/bin/node
-const args = process.argv.slice(2).map(Number);
-
-if (args.length <= 1) {
-    console.log(0);
-} else {
-    const sortedArgs = args.sort((a, b) => b - a);
-    console.log(sortedArgs[1]);
+function factorial(n) {
+  if (isNaN(n)) {
+    return 1; // Return 1 for NaN
+  }
+  if (n <= 1) {
+    return 1; // Base case: factorial(0) and factorial(1) are 1
+  }
+  return n * factorial(n - 1); // Recursive case
 }
+
+const input = process.argv[2]; // First argument from command line
+const num = parseInt(input, 10); // Cast the argument to an integer
+
+console.log(factorial(num));
